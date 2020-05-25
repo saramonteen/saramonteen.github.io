f6ecc87deddeed077391101e13b9a594fdfbf023
Sara Peek Monteen <saramonteen@gmail.com>
2020-05-25 16:29:15 -0700

    Moved the front page over to Website.js to free up index.js.

---
+++ src/Website.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Typing from 'react-typing-animation';
+import Resume from './pdfs/sara-peek-monteen-resume.pdf';
+import './stylesheet.css';
+
+const main = (
+    <div className="text">
+        <div>
+            <img className="header-image" src="./profile.png" alt="icon"/>
+        <Typing speed={100}>
+            Sara Peek Monteen
+            <Typing.Delay ms={500} />
+            <br/>
+            <br/>
+            <a className="link-text" href=" https://www.linkedin.com/in/sarapeekmonteen-6a2692125/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <br/>
+            <br/>
+            <a className="link-text" href={Resume} target="_blank" rel="noopener noreferrer">Resume</a>
+            <br/>
+            <br/>
+            <a className="link-text" href=" https://github.com/saramonteen" target="_blank" rel="noopener noreferrer">GitHub</a>
+        </Typing>
+        </div>
+    </div>
+);
+
+export default main;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,4 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import Typing from 'react-typing-animation';
-import Resume from './pdfs/sara-peek-monteen-resume.pdf';
-import './stylesheet.css';
+import Website from './Website.js';
 
-const main = (
-    <div className="text">
-        <div>
-            <img className="header-image" src="./profile.png" alt="icon"/>
-        <Typing speed={100}>
-            Sara Peek Monteen
-            <Typing.Delay ms={500} />
-            <br/>
-            <br/>
-            <a className="link-text" href=" https://www.linkedin.com/in/sarapeekmonteen-6a2692125/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-            <br/>
-            <br/>
-            <a className="link-text" href={Resume} target="_blank" rel="noopener noreferrer">Resume</a>
-            <br/>
-            <br/>
-            <a className="link-text" href=" https://github.com/saramonteen" target="_blank" rel="noopener noreferrer">GitHub</a>
-        </Typing>
-        </div>
-    </div>
-);
-
-
-ReactDOM.render(main, document.getElementById('root'));
+ReactDOM.render(Website, document.getElementById('root'));
