f979dfe6c69e3fad34511e67b810060981237c9a
Sara Peek Monteen <saramonteen@gmail.com>
2020-12-19 17:49:52 -0800

    Added San Diego specific weather information to the header.

--- src/WeatherHeader.jsx
+++ src/WeatherHeader.jsx
@@ -2,10 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './stylesheet.css';
 
 function Header(){
-
-    const [, setWeather] = useState([]);
-
-    // const [weather, setWeather] = useState([]);
+    const [weather, setWeather] = useState([]);
 
     var currentDate = new Date();
     var today = currentDate.toLocaleDateString('default', { weekday: 'long' }) + ", " + currentDate.toLocaleString('default', {month: 'long'}) + " " + currentDate.getDate() + ", " + currentDate.getFullYear();
@@ -15,13 +12,13 @@ function Header(){
             .then(res => res.json())
             .then(
                 (forecast) => {
-                    setWeather(forecast.periods);
+                    setWeather(forecast.properties.periods);
                 }
             )
-    }, [])
+    })
 
-    return (<div className='weather-header'>
-        {/* <div className="weather-header"> */}
+    return (<>
+        <div className='weather-header'>
             <div>
                 <img className="weather-header-image" src={require("./locationIcon.png")} alt="icon"/>
             </div>
@@ -29,21 +26,22 @@ function Header(){
                 San Diego, CA
                 <br/>
                 {today}
-            {/* </div> */}
-        </div>
-        <div className="forecast">
+            </div>
             
-            {/* <ul style={{listStyleType: 'none', margin: '0'}}> */}
-                {/* {weather.map(weatherForecast => ( */}
-                {/* <li key={weatherForecast.number}> */}
-                    {/* {weatherForecast.name} Forecast: */}
-                    {/* <br/> */}
-                    {/* {weatherForecast.detailedForecast} */}
-                {/* </li> */}
-                {/* ))} */}
-            {/* </ul> */}
-        </div>
-    </div>);
+            <div className="forecast">
+                <ul style={{listStyleType: 'none', margin: '0'}}>
+                    {weather && weather.map(weatherForecast => (
+                    <li key={weatherForecast.number}>
+                        <b>{weatherForecast.name}</b> {weatherForecast.detailedForecast}
+                        <br/>
+                    </li>
+                    ))}
+                </ul>
+            </div>
+        </div>    
+    </>);
+
 }
+
 
 export default Header;
